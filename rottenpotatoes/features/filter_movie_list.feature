c79de41d52db5d41dc5ea9def1d7c097b9ee7e10
Eleazar Meza <bairesdev_emeza@iseatz.com>
2018-08-19 02:42:36 -0400

    Implemented cucumber tests

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -1,5 +1,5 @@
 Feature: display list of movies filtered by MPAA rating
- 
+
   As a concerned parent
   So that I can quickly browse movies appropriate for my family
   I want to see movies matching only certain MPAA ratings
@@ -23,11 +23,10 @@ Background: movies have been added to database
   Then 10 seed movies should exist
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  When I check the following ratings: PG, R
+  And I uncheck the following ratings: G, PG-13, NC-17
+  And press "Refresh"
+  Then I should only see movies with checked ratings
 
 Scenario: all ratings selected
-  # see assignment
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,37 +2,38 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create movie
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
+  expect(Movie.count).to eq n_seeds.to_i
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  movie_titles = all("#movies tr td:nth-child(1)").map(&:text).select { |title| [e1, e2].include? title }
+  expect(movie_titles.first).to eq e1
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+When /^(?:|I )(un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  ratings_ids = rating_list.split(", ").map { |rating| "ratings_#{rating}" }
+  ratings_ids.each { |rating_id| uncheck ? uncheck(rating_id) : check(rating_id) }
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(all("#movies tbody tr").count).to eq Movie.count
+end
+
+Then(/^I should only see movies with checked ratings$/) do
+  checked_ratings = Movie.all_ratings.select { |rating| find("#ratings_#{rating}").checked? }
+  shown_ratings = all("#movies tr td:nth-child(2)").map(&:text).uniq
+
+  expect(shown_ratings).to match_array checked_ratings
 end
